ae450469a2c0d5ad5dea0cf1e26eb27d0ce8eabb
lgu33 <lgu33@wisc.edu>
2019-08-01 18:27:29 -0500

    added split table functionin db_builder

--- api/db_builder.py
+++ api/db_builder.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from sqlalchemy import *
-from flask_sqlalchemy import SQLAlchemy
-#from views import db
+import os
+from weather import Weather, Unit
+
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+data_path = os.path.join(curr_dir, 'data')
 
 
 def map_db_dataset():
@@ -14,53 +16,57 @@ def map_db_dataset():
 
     :return:
     """
-    primary_data = pd.read_csv('data/MERGED2017_18_PP.csv')
-    column_mappings = pd.read_excel('data/CollegeScorecardDataDictionary.xlsx', sheet_name="data_dictionary")
+
+    primary_data_path = os.path.join(data_path,  'MERGED2017_18_PP.csv')
+    primary_data = pd.read_csv(primary_data_path)
+    column_mappings_path = os.path.join(data_path, 'CollegeScorecardDataDictionary.xlsx')
+    column_mappings = pd.read_excel(column_mappings_path, sheet_name="data_dictionary")
     column_name_mapping = column_mappings[["developer-friendly name", "VARIABLE NAME"]].dropna()
     column_name_mapping_list = column_name_mapping["developer-friendly name"].tolist()
     primary_data.columns = column_name_mapping_list
-    primary_data.to_excel("mapped_data.xlsx")
+    mapped_data_path = os.path.join(data_path, 'mapped_data.xlsx')
+    primary_data.to_excel(mapped_data_path)
 
 
-def split_data_by_relation():
+def split_data_university_table(data):
     """
     A FUNCTION FOR READING IN THE MAPPED DATA (MAPPED_DATA.XLSX...PLEASE SEE FUNCTION ABOVE IF YOU DO NOT
-    HAVE THE FILE) AND SPLITTING THE DATA INTO THE NECESSARY RELATIONS.
+    HAVE THE FILE) AND SPLITTING THE DATA ONLY TO PRESERVE THOSE NECESSARY FOR UNIVERSITY.
 
 
     :return:
     """
-    data = pd.read_excel('data/mapped_data.xlsx')
+
     uni_data_range = list(range(23, 99))
     university_columns = [0, 1, 2, 3, 8, 9, 11, 12, 13, 14, 15] + uni_data_range
     university_data = data.iloc[:, university_columns]
 
-    return
+    return university_data
 
 
-class Users(db.Model):
+def split_data_local(data):
+    """
+    A FUNCTION FOR READING IN THE MAPPED DATA (MAPPED_DATA.XLSX...PLEASE SEE FUNCTION ABOVE IF YOU DO NOT
+    HAVE THE FILE) AND SPLITTING THE DATA ONLY TO PRESERVE THOSE NECESSARY FOR LOCATION. ALSO THIS FUNCTION
+    LOOKS UP THE WEATHER DATA
 
-    __tablename__ = "Users"
+    :return: data table for location
+    """
+    data_range = [0, 1, 2] + list(range(17, 25))
+    location_data = data.iloc[:, data_range]
+    weather = Weather(Unit.CELSIUS)
+    for index, row in location_data.iterrows():
+        longitude, latitude = row['location.lon'], row['location.lat']
+        lookup = weather.lookup_by_latlng(latitude, longitude)
+        curr_weather = lookup.condition
 
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String, unique=False, nullable=False)
-    last_name = db.Column(db.String, unique=False, nullable=False)
-    username = db.Column(db.String, unique=False, nullable=False)
-    password = db.Column(db.String, unique=False)
-    email = db.Column(db.String, unique=False, nullable=False)
-    birthdate = db.Column(db.Date, unique=False, nullable=False)
-    current_education_attainment = db.Column(db.String, unique=False, nullable=False)
-    joined_site = db.Column(db.Date, unique=False, nullable=False)
-
-
-class Comments(db.Model):
-
-    __tablename__ = "Comments"
-
-    id = db.Column(db.Integer, primary_key=True)
-    comment_text = db.Column(db.String)
+    return location_data
 
 
-db.create_all()
+mapped_data_path = os.path.join(data_path, 'mapped_data.xlsx')
+df_mapped_data = pd.read_excel(mapped_data_path)
 
+#df_university_table = split_data_university_table(df_mapped_data)
+df_location_table = split_data_local(df_mapped_data)
 
+print()
