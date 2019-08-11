2d2b932551ebe90000b30cb48a671097446a8d2c
Nik Burmeister <nburmeister@Niks-MacBook-Pro.local>
2019-08-11 14:20:00 -0500

    before flask imp

---
+++ api/views/search_db.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, request, make_response, jsonify
+from flask.views import MethodView
+from api.models import *
+
+
+class Search(MethodView):
+
+    def get(self):
+        pass
+
+
+search = Search.as_view("search_api")
+auth_blueprint.add_url_rule("/search",
+                            view_func=search,
+                            methods=["GET"])
